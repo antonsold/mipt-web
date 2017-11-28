e5799d0019676ee56efc63cd3267beccb88d46ad
antonsold <soldatenkovanton@mail.ru>
2017-11-28 15:29:13 +0300

    task 4

---
+++ task4/solve.go
@@ -0,0 +1,39 @@
+package task4
+
+import (
+	"strings"
+	"unicode"
+)
+
+func RemoveEven(slice []int) (ans []int) {
+	for i := range slice {
+		if slice[i] % 2 != 0 {
+			ans = append(ans, slice[i])
+		}
+	}
+	return ans
+}
+
+func PowerGenerator(base int) (func() int) {
+	val := 1
+	return func() (ans int) {
+		val *= base
+		ans = val
+		return
+	}
+}
+
+func DifferentWordsCount(text string) int {
+	runes := append([]rune(strings.ToLower(text)), rune(' '))
+	words := make(map[string]int)
+	lastIndex := -1
+	for idx, r := range runes {
+		if !unicode.IsLetter(r) {
+			if idx -lastIndex > 1 {
+				words[string(runes[lastIndex+ 1:idx])] = idx
+			}
+			lastIndex = idx
+		}
+	}
+	return len(words)
+}
